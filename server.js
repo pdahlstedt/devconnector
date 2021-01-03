03b19b7b4a883b7624ff96c648c72251d63c75fd
Petter Dahlstedt <petter.dahlstedt@outlook.com>
2021-01-03 19:02:04 +0100

    Added user and db

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const express = require("express");
+const express = require ('express');
+//import express from 'express'; TODO: fix package.json to allow typescript
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect db
+connectDB();
+
+// Init middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
