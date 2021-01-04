2573184a6cc7f0e9272bcf334ac04a26377adfb0
Petter Dahlstedt <petter.dahlstedt@outlook.com>
2021-01-04 12:42:33 +0100

    Added middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require("config");
 //const { check, validationResult} = require('express-validator/check');
 const {check, validationResult} = require('express-validator');
 
@@ -48,11 +50,24 @@ async (req,res) => {
 
         await user.save();
 
-        // Encrypt password
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
-        // Return jsonwebtoken
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            {expiresIn: 360000}, 
+            (err, token) => {
+                if( err) {
+                    throw err;
+                }
+                res.json({ token });
+            });
 
-        res.send('User registered');
+        //res.send('User registered');
 
     } catch(err) {
         console.error(err.message);
